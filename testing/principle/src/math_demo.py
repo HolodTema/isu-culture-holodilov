0bde688db859b24f8b9bf3aaeb8b6070c4ba3bba
artem <art.holodilov@gmail.com>
2026-09-22 23:13:52 +0300

    Started adding some test principles: that test-may-not-show-errors so we need to create appropriate tests

--- testing/principle/src/math_demo.py
+++ testing/principle/src/math_demo.py
@@ -2,3 +2,6 @@
 def add(a, b):
     return a + b
 
+def add_with_bug(a, b):
+    return a * b
+
--- testing/principle/tests/test_principles.py
+++ testing/principle/tests/test_principles.py
@@ -1,8 +1,33 @@
-from math_demo import add
+from math_demo import (
+    add,
+    add_with_bug
+)
+
+# Ранее тестирование позволяет съэкономить время позднее
+# Тесты показывают наличие ошибок, а не их отсутвие 
+# Тесты не должны дублировать логику тестируемого кода
+# Тесты не должны использовать ВСЕ наборы входных параметров
+# Тесты должны покрывать "кластеры" входных параметров
+# Тесты должны обнаруживать новые ошибки (pescicide paradox)
+# Тесты покрывают как успешные так и ошибочные кейсы
 
 def test_addition():
-    assert 2 + 2 == 4
+    assert add(2, 2) == 4
+    assert add(0, 0) == 0
+    assert add(7, 6) == 13
+    print("Test addition is passed")
+
+def test_addition_with_bug():
+    # Тесты показывают наличие ошибок, а не их отсутвие 
+    # то есть эта функция это ложноположительный тест, ибо ошибки не нашел
+    assert add_with_bug(2, 2) == 4
+    assert add_with_bug(0, 0) == 0
+    print("Test addition with bug is passed")
+    # finally we found data that make test reliable
+    # если бы мы написали строчку ниже, мы бы нашли баг этим тестом
+    # assert add_with_bug(7, 6) == 13 # will fail here
 
 if __name__ == "__main__":
     test_addition()
+    test_addition_with_bug()
 
