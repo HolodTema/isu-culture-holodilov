128694d961d6a02b42a666e771f0c3b1400a638a
artem <art.holodilov@gmail.com>
2026-09-23 00:32:25 +0300

    Modified ndfl.py to pass all the unit-tests

--- testing/tdd/src/ndfl.py
+++ testing/tdd/src/ndfl.py
@@ -1,3 +1,29 @@
 def calculate_ndfl(income):
-    return None
+    result = 0
+    if income < 2_400_000:
+        result = income * 0.13
+    elif income < 5_000_000:
+        result = 2_400_000 * 0.13 + (income - 2_400_000) * 0.15
+    elif income < 20_000_000:       
+        result = (
+            2_400_000 * 0.13 + 
+            2_600_000 * 0.15 +
+            (income - 5_000_000) * 0.18
+        )
+    elif income < 50_000_000:       
+        result = (
+            2_400_000 * 0.13 + 
+            2_600_000 * 0.15 +
+            15_000_000 * 0.18 +
+            (income - 20_000_000) * 0.20
+        )
+    elif income > 50_000_000:       
+        result = (
+            2_400_000 * 0.13 + 
+            2_600_000 * 0.15 +
+            15_000_000 * 0.18 +
+            30_000_000 * 0.20 +
+            (income - 50_000_000) * 0.22
+        )
+    return result
 
