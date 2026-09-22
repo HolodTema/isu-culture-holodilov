8f0266869d2fe5fb4af8c7435c2e5ecf2fb0a02d
artem <art.holodilov@gmail.com>
2026-09-23 01:44:50 +0300

    sales.py _parse_record(): added docstring to auto-generate docs later

--- docs_domain/src/sales.py
+++ docs_domain/src/sales.py
@@ -1,5 +1,19 @@
 
-def _parse_record(line: str):
+def _parse_record(line: str) -> dict | None: 
+    """Parsing information for one sale.
+
+    Parameters:
+        line - string that contain sale record in form of comma-separated values
+
+    Returns:
+        sale - sale information in form of dict
+
+    Raises:
+        ValueError
+    """
+    if line == "":
+        raise ValueError("Got zero length line")
+    
     sale = line.strip().split(",")
     if len(sale) != 4:
         return None
