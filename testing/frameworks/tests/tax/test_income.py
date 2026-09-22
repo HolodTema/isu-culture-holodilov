77f233f04f89190f0f666f0b0fbfca9fec5de208
artem <art.holodilov@gmail.com>
2026-09-23 00:16:26 +0300

    Unit test using pytest lib

--- testing/frameworks/tests/tax/test_income.py
+++ testing/frameworks/tests/tax/test_income.py
@@ -1,9 +1,9 @@
 from tax.income import calculate_tax
 
 def test_income_tax():
-    assert calculate_tax(100) == 15
+    assert calculate_tax(100) == 13
     print("Test tax passed")
 
 if __name__ == "__main__":
-    test_income_tax()A
+    test_income_tax()
 
